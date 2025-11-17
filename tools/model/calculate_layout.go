830adef6354027d0529d4038f4b9f4a3fef8e338
dylanwglenn <dylanwglenn@gmail.com>
2025-11-17 18:16:50 +0100

    avoid ege case panic

--- tools/model/calculate_layout.go
+++ tools/model/calculate_layout.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"main/model"
 	"main/utils"
 	"math"
 	"slices"
 	"sort"
 
 	"gioui.org/layout"
-	"gioui.org/op"
-	"gioui.org/unit"
 )
 
 // edge numbering for rectangles
@@ -23,16 +20,17 @@ import (
 //   ----------------------------
 //                2
 
-var varianceRadius float32 = 20
+var VarianceRadius float32 = 20
+var targetPadding float32 = 10
 
-func CalculateModel(m *model.Model, gtx layout.Context) {
+func CalculateModel(m *Model, gtx layout.Context) {
 	// Reset all node connections every frame
 	for _, n := range m.Nodes {
 		if !n.Visible {
 			continue
 		}
 
-		n.EdgeConnections = [4][]*model.Connection{}
+		n.EdgeConnections = [4][]*Connection{}
 		// define node dimensions
 		if n.TextWidth == 0 {
 			n.TextWidth = utils.GetTextWidth(n.Text, m.Font.Face, m.Font.Size, gtx)
@@ -42,11 +40,11 @@ func CalculateModel(m *model.Model, gtx layout.Context) {
 		adjWidth := n.TextWidth + targetPadding*2
 		n.Padding = (adjWidth - n.TextWidth) / 2.0
 		switch n.Class {
-		case model.OBSERVED:
+		case OBSERVED:
 			n.Dim = utils.LocalDim{W: adjWidth, H: 50}
-		case model.LATENT:
+		case LATENT:
 			n.Dim = utils.LocalDim{W: adjWidth, H: adjWidth}
-		case model.INTERCEPT:
+		case INTERCEPT:
 			//todo: handle intercepts
 		}
 	}
@@ -71,9 +69,9 @@ func CalculateModel(m *model.Model, gtx layout.Context) {
 		}
 
 		switch c.Type {
-		case model.STRAIGHT:
+		case STRAIGHT:
 			c.Angle = utils.GetAngleLoc(c.Origin.Pos, c.Destination.Pos)
-		case model.CURVED:
+		case CURVED:
 			ctrl := utils.GetCtrlPoint(c.Origin.Pos.ToF32(), c.Destination.Pos.ToF32(), c.Curvature)
 			angle := -math.Atan2(float64(ctrl.Y-c.Origin.Pos.ToF32().Y), float64(ctrl.X-c.Origin.Pos.ToF32().X))
 			c.Angle = utils.NormalizeAngle(angle)
@@ -91,7 +89,7 @@ func CalculateModel(m *model.Model, gtx layout.Context) {
 		}
 
 		switch n.Class {
-		case model.OBSERVED:
+		case OBSERVED:
 			// handle connections
 			for e := 0; e < 4; e++ {
 				connections := n.EdgeConnections[e]
@@ -101,17 +99,17 @@ func CalculateModel(m *model.Model, gtx layout.Context) {
 				}
 
 				// sort connections based on angle
-				angleMap := make(map[*model.Connection]float64, len(connections))
+				angleMap := make(map[*Connection]float64, len(connections))
 				for _, c := range connections {
 					switch c.Type {
-					case model.STRAIGHT:
+					case STRAIGHT:
 						if c.Destination == n {
 							angleMap[c] = c.Angle
 						}
 						if c.Origin == n {
 							angleMap[c] = utils.NormalizeAngle(c.Angle + math.Pi)
 						}
-					case model.CURVED:
+					case CURVED:
 						if c.Origin == n {
 							angleMap[c] = utils.GetAngleLoc(c.Destination.Pos, c.Origin.Pos)
 						}
@@ -178,34 +176,34 @@ func CalculateModel(m *model.Model, gtx layout.Context) {
 		}
 
 		switch {
-		case c.Type != model.CIRCULAR:
-			if c.Origin.Class == model.LATENT {
+		case c.Type != CIRCULAR:
+			if c.Origin.Class == LATENT {
 				angleFromLatent := utils.GetAngleLoc(c.Origin.Pos, c.DestinationPos)
 				c.OriginPos = utils.MoveAlongAngleLoc(c.Origin.Pos, angleFromLatent, c.Origin.Dim.W/2.0)
 			}
-			if c.Destination.Class == model.LATENT {
+			if c.Destination.Class == LATENT {
 				angleToLatent := utils.GetAngleLoc(c.OriginPos, c.Destination.Pos)
 				c.DestinationPos = utils.MoveAlongAngleLoc(c.Destination.Pos, angleToLatent+math.Pi, c.Destination.Dim.W/2.0)
 			}
 
 			// determine label position as distance along curve
 			switch c.Type {
-			case model.STRAIGHT:
+			case STRAIGHT:
 				angle := utils.GetAngleLoc(c.OriginPos, c.DestinationPos)
 				dist := utils.DistLoc(c.OriginPos, c.DestinationPos)
 				c.EstPos = utils.MoveAlongAngleLoc(c.OriginPos, angle, dist*c.AlongLineProp)
-			case model.CURVED:
+			case CURVED:
 				ctrl := utils.GetCtrlPoint(c.OriginPos.ToF32(), c.DestinationPos.ToF32(), c.Curvature)
 				c.EstPos = utils.MoveAlongBezier(c.OriginPos.ToF32(), c.DestinationPos.ToF32(), ctrl, c.AlongLineProp)
 			}
 		default: // case connection is circular
 			switch c.Origin.Class {
-			case model.LATENT:
+			case LATENT:
 				r := c.Origin.Dim.W / 2.0
 				c.OriginPos = utils.MoveAlongAngleLoc(c.Origin.Pos, utils.NormalizeAngle(c.VarianceAngle-math.Pi/8), r)
 				c.DestinationPos = utils.MoveAlongAngleLoc(c.Origin.Pos, utils.NormalizeAngle(c.VarianceAngle+math.Pi/8), r)
 				c.RefPos = utils.MoveAlongAngleLoc(c.Origin.Pos, c.VarianceAngle, r)
-			case model.OBSERVED:
+			case OBSERVED:
 				angleOrigin := utils.NormalizeAngle(c.VarianceAngle - math.Pi/8)
 				angleDestination := utils.NormalizeAngle(c.VarianceAngle + math.Pi/8)
 
@@ -213,124 +211,25 @@ func CalculateModel(m *model.Model, gtx layout.Context) {
 				c.DestinationPos = utils.AngleRectIntersection(angleDestination, c.Origin.Pos, c.Origin.Dim)
 				c.RefPos = utils.AngleRectIntersection(c.VarianceAngle, c.Origin.Pos, c.Origin.Dim.Add(utils.LocalDim{5, 5}))
 			}
-			circleCenter := utils.FindCircleCenter(c.OriginPos.ToF32(), c.DestinationPos.ToF32(), c.RefPos.ToF32(), varianceRadius)
-			c.EstPos = utils.MoveAlongAngleLoc(utils.ToLocalPos(circleCenter), c.VarianceAngle, varianceRadius)
+			circleCenter := utils.FindCircleCenter(c.OriginPos.ToF32(), c.DestinationPos.ToF32(), c.RefPos.ToF32(), VarianceRadius)
+			c.EstPos = utils.MoveAlongAngleLoc(utils.ToLocalPos(circleCenter), c.VarianceAngle, VarianceRadius)
 		}
 	}
 }
 
-// DrawModel draws the path diagram
-func DrawModel(ops *op.Ops, gtx layout.Context, m *model.Model, ec *EditContext) {
-	for _, n := range m.Nodes {
-		if !n.Visible {
-			continue
-		}
-
-		switch n.Class {
-		case model.OBSERVED:
-			utils.DrawRect(
-				ops,
-				n.Pos.ToGlobal(ec.scaleFactor, ec.viewportCenter, ec.windowSize),
-				n.Dim.ToGlobal(ec.scaleFactor),
-				n.Col,
-				n.Thickness*ec.scaleFactor,
-			)
-		case model.LATENT:
-			utils.DrawEllipse(
-				ops,
-				n.Pos.ToGlobal(ec.scaleFactor, ec.viewportCenter, ec.windowSize),
-				n.Dim.ToGlobal(ec.scaleFactor),
-				n.Col,
-				n.Thickness*ec.scaleFactor,
-			)
-		}
-
-		textOffset := utils.LocalDim{W: n.Dim.W/2.0 - n.Padding, H: m.Font.Size / 1.5} // I think 1.5 is a magic number
-		utils.DrawText(
-			ops,
-			gtx,
-			n.Pos.SubDim(textOffset).ToGlobal(ec.scaleFactor, ec.viewportCenter, ec.windowSize),
-			n.Text,
-			m.Font.Face,
-			unit.Sp(m.Font.Size),
-			ec.scaleFactor,
-		)
-	}
-
-	for _, c := range m.Connections {
-		if !c.UserDefined && !m.ViewGenerated {
-			continue
-		}
-
-		switch c.Type {
-		case model.STRAIGHT:
-			utils.DrawArrowLine(
-				ops,
-				c.OriginPos.ToGlobal(ec.scaleFactor, ec.viewportCenter, ec.windowSize),
-				c.DestinationPos.ToGlobal(ec.scaleFactor, ec.viewportCenter, ec.windowSize),
-				c.Col,
-				c.Thickness*ec.scaleFactor,
-				ec.windowSize,
-			)
-		case model.CURVED:
-			utils.DrawArrowCurve(
-				ops,
-				c.OriginPos.ToGlobal(ec.scaleFactor, ec.viewportCenter, ec.windowSize),
-				c.DestinationPos.ToGlobal(ec.scaleFactor, ec.viewportCenter, ec.windowSize),
-				c.Col,
-				c.Thickness*ec.scaleFactor,
-				c.Curvature,
-				ec.windowSize,
-			)
-		case model.CIRCULAR:
-			utils.DrawArrowArc(
-				ops,
-				c.OriginPos.ToGlobal(ec.scaleFactor, ec.viewportCenter, ec.windowSize),
-				c.DestinationPos.ToGlobal(ec.scaleFactor, ec.viewportCenter, ec.windowSize),
-				c.RefPos.ToGlobal(ec.scaleFactor, ec.viewportCenter, ec.windowSize),
-				varianceRadius*ec.scaleFactor,
-				c.Col,
-				c.Thickness*ec.scaleFactor,
-				ec.windowSize,
-			)
-		}
-	}
-	// draw estimate labels after ALL of the connections to ensure proper layering
-	for _, c := range m.Connections {
-		if !c.UserDefined && !m.ViewGenerated {
-			continue
-		}
-
-		if m.CoeffDisplay != utils.NONE {
-			utils.DrawEstimate(
-				ops,
-				gtx,
-				c.EstPos.ToGlobal(ec.scaleFactor, ec.viewportCenter, ec.windowSize),
-				m.Font.Face,
-				m.Font.Size,
-				ec.scaleFactor,
-				c.EstPadding,
-				c.EstText,
-				c.EstDim,
-				c.EstWidth,
-			)
-		}
-	}
-}
-
-func AssignToEdges(c *model.Connection, nodes []*model.Node) {
+func AssignToEdges(c *Connection, nodes []*Node) {
 	switch {
-	case c.Origin.Class == model.OBSERVED && c.Destination.Class == model.LATENT:
+	case c.Origin.Class == OBSERVED && c.Destination.Class == LATENT:
 		candidateOriginEdges := GetCandidateDestEdges(InvertAngle(c))
 		edgeOrigin := GetBestEdge(candidateOriginEdges, c.Origin, c.Destination, c.Destination.Pos, nodes)
 		c.Origin.EdgeConnections[edgeOrigin] = append(c.Origin.EdgeConnections[edgeOrigin], c)
 
-	case c.Origin.Class == model.LATENT && c.Destination.Class == model.OBSERVED:
+	case c.Origin.Class == LATENT && c.Destination.Class == OBSERVED:
 		candidateOriginEdges := GetCandidateDestEdges(c.Angle)
 		edgeDest := GetBestEdge(candidateOriginEdges, c.Destination, c.Origin, c.Origin.Pos, nodes)
 		c.Destination.EdgeConnections[edgeDest] = append(c.Destination.EdgeConnections[edgeDest], c)
 
-	case c.Origin.Class == model.OBSERVED && c.Destination.Class == model.OBSERVED:
+	case c.Origin.Class == OBSERVED && c.Destination.Class == OBSERVED:
 		// assign origin edge like normal
 		edgeOrigin := AngleToEdge(c.Angle)
 		c.Origin.EdgeConnections[edgeOrigin] = append(c.Origin.EdgeConnections[edgeOrigin], c)
@@ -346,7 +245,7 @@ func AssignToEdges(c *model.Connection, nodes []*model.Node) {
 
 }
 
-func GetBestEdge(candidateEdges []int, destNode, originNode *model.Node, originEdgePoint utils.LocalPos, nodes []*model.Node) int {
+func GetBestEdge(candidateEdges []int, destNode, originNode *Node, originEdgePoint utils.LocalPos, nodes []*Node) int {
 	// best edge is defined as the edge where the line angle is maximally different from
 	// the edge angle (the edge angle being either flat or 90* based on the edge)
 	// OR
@@ -364,7 +263,7 @@ func GetBestEdge(candidateEdges []int, destNode, originNode *model.Node, originE
 			angles[i] = math.Abs(math.Cos(angle))
 		}
 
-		intersections[i] = checkIntersection(originEdgePoint, destEdgePoint, nodes, []*model.Node{destNode, originNode})
+		intersections[i] = checkIntersection(originEdgePoint, destEdgePoint, nodes, []*Node{destNode, originNode})
 	}
 
 	switch {
@@ -391,19 +290,19 @@ func GetCandidateDestEdges(angle float64) []int {
 		return []int{1, 2}
 	case angle >= math.Pi && angle < 3*math.Pi/2.0:
 		return []int{0, 1}
-	case angle >= 3*math.Pi/2.0 && angle < 2*math.Pi:
+	case angle >= 3*math.Pi/2.0 && angle <= 2*math.Pi:
 		return []int{3, 0}
 	default:
 		panic("invalid angle value")
 	}
 }
 
-func InvertAngle(c *model.Connection) float64 {
+func InvertAngle(c *Connection) float64 {
 	var angle float64
 	switch c.Type {
-	case model.STRAIGHT:
+	case STRAIGHT:
 		angle = utils.NormalizeAngle(c.Angle + math.Pi)
-	case model.CURVED:
+	case CURVED:
 		ctrl := utils.GetCtrlPoint(c.Origin.Pos.ToF32(), c.Destination.Pos.ToF32(), c.Curvature)
 		angle = -math.Atan2(float64(ctrl.Y-c.Destination.Pos.ToF32().Y), float64(ctrl.X-c.Destination.Pos.ToF32().X))
 		angle = utils.NormalizeAngle(angle)
@@ -426,8 +325,8 @@ func AngleToEdge(angle float64) int {
 	}
 }
 
-func SubdivideNodeEdge(n *model.Node, edge, numPoints int) []utils.LocalPos {
-	if n.Class != model.OBSERVED {
+func SubdivideNodeEdge(n *Node, edge, numPoints int) []utils.LocalPos {
+	if n.Class != OBSERVED {
 		return nil
 	}
 
@@ -478,7 +377,7 @@ func SubdivideNodeEdge(n *model.Node, edge, numPoints int) []utils.LocalPos {
 	return res
 }
 
-func checkIntersection(originPos, destPos utils.LocalPos, nodes, exclusion []*model.Node) bool {
+func checkIntersection(originPos, destPos utils.LocalPos, nodes, exclusion []*Node) bool {
 	for _, n := range nodes {
 		if slices.Contains(exclusion, n) {
 			continue
